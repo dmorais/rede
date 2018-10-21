395a3fe3811d481df8e5ad408157ae761540ae9b
DaveMorais <dmorais@ccs.usherbrooke.ca>
2018-10-21 09:47:35 -0400

    Added name normalization

--- util/crawler.py
+++ util/crawler.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+import re
 
 def get_publication(list_authors):
     '''
@@ -23,13 +23,90 @@ def get_publication(list_authors):
                     unwanted_ano = div_layout5.find('span', {'data-tipo-ordenacao': 'ano'})
                     unwanted_ano.extract()
 
-                    splitter(record[0], div_layout5.getText(separator=u' ').strip())
+                    _splitter(record[0], div_layout5.getText(separator=u' ').strip())
     return True
 
 
-def splitter(author, pub):
+def _splitter(author, pub):
     record = pub.strip().split(' . ')
     title = record[1].split('.')
-    print(author + "|" + record[0] + "|" + title[0] + "| " + "".join(title[1:]))
+    _normalize_names(author + "|" + record[0] + "|" + title[0] + "| " + "".join(title[1:]))
 
     return True
+
+
+
+def _normalize_names(pub):
+    '''
+
+    :param pub: string in the format specified by the function splitter
+    :return:
+    '''
+
+    regexes = (
+        r' D[AEIOU]\s+',
+        r'-',
+        r'DOS',
+        r'DAS'
+
+    )
+
+    regex = re.compile("|".join(regexes), re.IGNORECASE)
+    record = pub.strip().split("|")
+
+    authors = record[1].split(';')
+    authors_corrected = list()
+
+
+    for author in authors:
+
+        # Capitalize and Remove preposition and hifens from names
+        author = re.sub(regex, " ", author.strip().upper()).strip()
+
+        # Convert Author to the format Last_name, Initials.
+        names = author.split(',')
+
+        # Check if last_name is made up by two names
+        last_names = names[0].split(' ')
+
+        # Convert last_name last_name, name to last_name, name last_name
+        if len(last_names) > 1:
+            names[0] = last_names[-1]
+            names[1] += " " + "  ".join(last_names[:-1])
+
+        # Fix cases where fisrt_names appear without space M.G
+        names[1] = names[1].replace('.', '. ')
+
+        first_names = names[1].strip().split(' ')
+        list_of_correct_names = list()
+
+
+        for first_name in first_names:
+
+            if first_name == '':
+                continue
+            else:
+                if len(first_name) == 1:
+                    first_name += '.'
+                    list_of_correct_names.append(first_name)
+                elif len(first_name) > 2:
+                    first_name = first_name[0] + '.'
+                    list_of_correct_names.append(first_name)
+                else:
+                    list_of_correct_names.append(first_name)
+
+        names[1] = " ".join(list_of_correct_names)
+
+        names_corrected = list()
+        names_corrected.append(" ".join(names))
+
+        # put Everything back (correcte list of names)
+        authors_corrected.append("".join(names_corrected))
+
+    # put Everything back (correcte list of records)
+    record[1] = "; ".join(authors_corrected)
+
+    print("|".join(record), end="\n\n")
+
+    return True
+
